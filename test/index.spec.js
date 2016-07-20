28ad10fa00befcb79281208999cb33cf4b012e1d
Kai Curry <kai@cape.io>
2016-07-19 21:10:24 -0500

    really basic tests.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,20 @@
 import test from 'tape'
 import humps from '../'
+import { getVal, humpsObj, humpsArr } from '../src/lodash-humps'
 
+test('getVal', t => {
+  t.equal(getVal('foo'), 'foo', 'non object')
+  t.deepEqual(getVal({ foo_bar: 'underscore' }), { fooBar: 'underscore' }, 'object')
+  t.end()
+})
+test('humpsObj', t => {
+  t.deepEqual(humpsObj({ foo_bar: 'underscore' }), { fooBar: 'underscore' }, 'basic')
+  t.end()
+})
+test('humpsArr', t => {
+  t.deepEqual(humpsArr([ { foo_bar: 'underscore' } ]), [ { fooBar: 'underscore' } ], 'basic')
+  t.end()
+})
 test('humps', t => {
   t.equal(humps('some string_yo-dance around'), 'someStringYoDanceAround', 'strings')
   const before = {
