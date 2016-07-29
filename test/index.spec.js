22d3a9b3c8e359e702a9ec7af6da2529cede526c
Kai Curry <kai@cape.io>
2016-07-28 20:51:19 -0500

    added tests for caps and more text fields

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,13 +3,21 @@ import humps from '../'
 
 test('humps', t => {
   const before = {
-    'space key': 'space', underscore_key: { another_one: 2 },
+    'space key': 'space',
+    underscore_key: { another_one: 2 },
+    TXT: 'ONE MORE THING',
+    foo_bar: 'underscore',
+    tests: 'ASTM D 4157-13',
   }
   const after = {
-    spaceKey: 'space', underscoreKey: { anotherOne: 2 },
+    spaceKey: 'space',
+    underscoreKey: { anotherOne: 2 },
+    txt: 'ONE MORE THING',
+    fooBar: 'underscore',
+    tests: 'ASTM D 4157-13',
   }
   t.deepEqual(humps(before), after, 'objects')
-  t.deepEqual(humps([ { foo_bar: 'underscore' } ]), [ { fooBar: 'underscore' } ], 'array')
+  t.deepEqual(humps([ before, before ]), [ after, after ], 'array')
   t.equal(humps('some string_yo-dance around'), 'someStringYoDanceAround', 'strings')
   t.end()
 })
