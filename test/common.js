5f455e4477d47915f8c4b1eb66482354002697e6
Kai Curry <kai@cape.io>
2017-06-20 14:09:48 -0400

    no longer used

--- test/common.js
+++
@@ -1,7 +0,0 @@
-const test = require('tape')
-const humps = require('../')
-
-test('humps', function(t) {
-  t.equal(humps('some string_yo-dance around'), 'someStringYoDanceAround', 'strings')
-  t.end()
-})
