1af1e9a0850a56f58a3aa86a47194ac43f38b042
Kai Curry <kai@cape.io>
2017-06-20 14:09:39 -0400

    demonstrate v2 kills functions

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import test from 'tape'
 import humps from '../'
 
@@ -9,6 +10,7 @@ test('humps', (t) => {
     foo_bar: 'underscore',
     tests: 'ASTM D 4157-13',
     contents: [ '100% Acrylic' ],
+    'dispatch-func': _.noop,
   }
   const after = {
     spaceKey: 'space',
@@ -17,9 +19,12 @@ test('humps', (t) => {
     fooBar: 'underscore',
     tests: 'ASTM D 4157-13',
     contents: [ '100% Acrylic' ],
+    dispatchFunc: {},
   }
   t.equal(humps('some string_yo'), 'some string_yo', 'no string processing')
   t.deepEqual(humps(before), after, 'objects')
   t.deepEqual(humps([ before, before ]), [ after, after ], 'array')
+  t.deepEqual(humps(_.noop), {}, 'kills functions')
+  t.deepEqual(humps(Date), {}, 'date class')
   t.end()
 })
